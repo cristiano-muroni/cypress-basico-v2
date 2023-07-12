5ab5613c28043285ce503e77a521f69ca82fef8a
Cristiano da Silva Muroni <cristiano.muroni@grupoboticario.com.br>
2023-07-12 12:23:49 -0300

    marca cada tipo de atendimento utilizando wrap e each

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -102,9 +102,24 @@ describe("Central de Atendimento ao Cliente TAT", () => {
       .should('have.value', 'mentoria')
   })
 
-  it.only('Seleciona um produto (Blog) por seu índice', () => {
+  it('Seleciona um produto (Blog) por seu índice', () => {
     cy.get('#product')
       .select(1)
       .should('have.value', 'blog')
+  })
+
+  it('marca o tipo de atendimento "Feedback"', () => {
+    cy.get('input[type="radio"][value="feedback"]')
+      .check()
+      .should('have.value', 'feedback')
+  })
+
+  it.only('marca cada tipo de atendimento', () => {
+    cy.get('input[type="radio"]')
+      .should('have.length', 3)
+      .each(($radio) => {
+        cy.wrap($radio).check()
+        cy.wrap($radio).should('be.checked')
+      })
   })
 });
